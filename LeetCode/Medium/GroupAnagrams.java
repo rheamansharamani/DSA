8b93adc79f6fd647e16096f51fcee16f3d13b6f0
rheamansharamani <rheamansharamani@gmail.com>
2025-09-07 19:11:33 +0530

    GroupAnagrams.java

---
+++ LeetCode/Medium/GroupAnagrams.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+    if (strs == null || strs.length == 0)
+      return new ArrayList<>();
+
+    Map<String, List<String>> stringAnagramsMap = new HashMap<>();
+    for (String s : strs) {
+      char[] arr = s.toCharArray();
+      Arrays.sort(arr);
+      String key = String.valueOf(arr);
+
+      if (!stringAnagramsMap.containsKey(key))
+        stringAnagramsMap.put(key, new ArrayList<>());
+
+      stringAnagramsMap.get(key).add(s);
+    }
+    return new ArrayList<>(stringAnagramsMap.values());
+  }
+}
