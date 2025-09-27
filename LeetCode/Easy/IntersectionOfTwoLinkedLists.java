8874a46283127135505045c5167eddb2e3320388
rheamansharamani <rheamansharamani@gmail.com>
2025-09-27 11:06:40 +0530

    IntersectionOfTwoLinkedLists.java

---
+++ LeetCode/Easy/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null) return headA;
+        if(headB == null) return headB;
+
+        int lenA = getListLength(headA);
+        int lenB = getListLength(headB);
+
+        while(lenA > lenB){
+            lenA--;
+            headA = headA.next;
+        }
+
+        while(lenB > lenA){
+            lenB--;
+            headB = headB.next;
+        }
+
+        while(headA != headB){
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+
+    private int getListLength(ListNode head){
+        int count = 0;
+        while(head != null){
+            head = head.next;
+            count++;
+        }
+        return count;
+    }
+}
