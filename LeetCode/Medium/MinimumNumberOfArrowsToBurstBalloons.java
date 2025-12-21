d548e30ebff09ec745e6407062610fa24c572394
rheamansharamani <rheamansharamani@gmail.com>
2025-12-21 19:07:22 +0530

    Q452. Minimum Number Of Arrows To Burst Balloons

---
+++ LeetCode/Medium/MinimumNumberOfArrowsToBurstBalloons.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        int arrows = 1;
+        if (points.length == 0) return 0;
+        Arrays.sort(points, (a,b) -> Integer.compare(a[0], b[0]));
+
+        int prevStart = points[0][0];
+        int prevEnd = points[0][1];
+
+        for(int i = 1; i < points.length; i++){
+
+            int currStart = points[i][0];
+            int currEnd = points[i][1];
+            if(prevEnd < currStart){
+                arrows++;
+                prevStart = currStart;
+                prevEnd = currEnd;
+            }
+            else{
+                prevStart = Math.max(prevStart, currStart);
+                prevEnd = Math.min(prevEnd, currEnd);
+            }
+        }
+        return arrows;
+    }
+}
